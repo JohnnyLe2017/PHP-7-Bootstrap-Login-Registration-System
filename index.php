cd5a6f53450ab625debbae6feb9f532ac4b43c34
JohnnyLe2017 <lejohnn2@gmail.com>
2018-03-09 20:32:13 -0500

    Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+  <?php include ("includes/header.php") ?>
+  <?php include ("includes/navigation.php") ?>
+	 
+	
+
+
+
+	<div class="jumbotron">
+		<h1 class="text-center"> Home Page</h1>
+	</div>
+
+
+	
