41f2daf50975c830ec8447c21d80576e82a543ae
JohnnyLe2017 <lejohnn2@gmail.com>
2018-03-09 21:59:39 -0500

    Commit

--- functions/functions.php
+++ functions/functions.php
@@ -7,11 +7,29 @@ function clean($string) {
 	return htmlentities($string);
 }
 
-function redirect() {
-
+function redirect($location) {
+	return header("Location: {$location}");
 }
 
+function set_message($message) {
+  if(!empty($message)) {
+    $_SESSION['message'] = $message;
+  } else {
+    $message = "";
+  }
+}
 
+function display_message() {
+  if(isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+     unset( $_SESSION['message']);
+  }
+}
+
+//function to make forms very secure using MD5
+function token_generator() {
+ md5(uniqid(mt_rand(), true));
+}
 
 
 
