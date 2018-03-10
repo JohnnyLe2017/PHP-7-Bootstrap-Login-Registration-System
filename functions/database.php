51e6e422ce9986446b5efdff4f3fe7c9f43760fd
JohnnyLe2017 <lejohnn2@gmail.com>
2018-03-09 21:39:32 -0500

    Commit

--- functions/database.php
+++ functions/database.php
@@ -15,6 +15,13 @@ global $con;
 return mysqli_query($con, $query);
 }
 
+function confirm($result) {
+	global $con;
+	if(!$result) {
+		die("QUERY FAILED", mysqli_error($con));
+	}
+}
+
 function fetch_array($result) {
 	global $con;
 	 return mysqli_fetch_array($result);
