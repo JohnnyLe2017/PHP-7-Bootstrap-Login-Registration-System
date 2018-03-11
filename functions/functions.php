283654151acb367cc2c671436d354142e249d2cb
JohnnyLe2017 <lejohnn2@gmail.com>
2018-03-11 17:51:36 -0400

    Commit

--- functions/functions.php
+++ functions/functions.php
@@ -186,27 +186,27 @@ function register_user($first_name, $last_name, $username, $email, $password) {
 
 function activate_user() {
 
-if($_SERVER['REQUEST_METHOD'] == "GET") {
-if(isset($_GET['email'])) {
-$email = clean($_GET['email']);
-$validation_code = clean($_GET['code']);
-$sql = "SELECT id FROM users WHERE email = '".escape($_GET['email'])."' AND validation_code = '".escape($_GET['code'])."' ";
-$result = query($sql);
-confirm($result);
+	if($_SERVER['REQUEST_METHOD'] == "GET") {
+	if(isset($_GET['email'])) {
+	$email = clean($_GET['email']);
+	$validation_code = clean($_GET['code']);
+	$sql = "SELECT id FROM users WHERE email = '".escape($_GET['email'])."' AND validation_code = '".escape($_GET['code'])."' ";
+	$result = query($sql);
+	confirm($result);
 
-if(row_count($result) == 1) {
-$sql2 = "UPDATE users SET active = 1, validation_code = 0 WHERE email = '".escape($email)."' AND validation_code = '".escape($validation_code)."' ";
-$result2 = query($sql2);
-confirm($result2);
+	if(row_count($result) == 1) {
+	$sql2 = "UPDATE users SET active = 1, validation_code = 0 WHERE email = '".escape($email)."' AND validation_code = '".escape($validation_code)."' ";
+	$result2 = query($sql2);
+	confirm($result2);
 
-set_message("<p class='bg-success'>Your account has been activated. You may now login.</p>");
-redirect("login.php");
-} else {
-set_message("<p class='bg-danger'>Your account could not be activated.</p>");
-redirect("login.php");
-			}
-		} 
-	}
+	set_message("<p class='bg-success'>Your account has been activated. You may now login.</p>");
+	redirect("login.php");
+	} else {
+	set_message("<p class='bg-danger'>Your account could not be activated.</p>");
+	redirect("login.php");
+				}
+			} 
+		}
 }
 
 
